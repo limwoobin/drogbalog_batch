169fc082a051972b12ce8ee7d98a4fa27364938c
limwoobin <drogba02@naver.com>
2021-02-01 17:09:36 +0900

    Fix - jobParameter bean 설정 수정
    jobParameter Bean 주입 클래스 수정
    Bean 불러오기 코드 작성

--- src/main/java/com/drogbalog/batch/visit/job/VisitJobConfig.java
+++ src/main/java/com/drogbalog/batch/visit/job/VisitJobConfig.java
@@ -8,43 +8,37 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 @Log4j2
 @RequiredArgsConstructor
 @Configuration
 public class VisitJobConfig {
+    public static final String JOB_NAME = "visitorJob";
+
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
     private final VisitorTasklet visitorTasklet;
-//    private final CreateDateJobParameter jobParameter;
 
-//    @Bean
-//    @JobScope
-//    public CreateDateJobParameter jobParameter(@Value("#{jobParameters[createDate]}") String createDate) {
-//        return new CreateDateJobParameter(createDate);
-//    }
+    @Bean(JOB_NAME + "jobParameter")
+    @JobScope
+    public CreateDateJobParameter jobParameter() {
+        return new CreateDateJobParameter();
+    }
 
-    @Bean
+    @Bean(name = JOB_NAME)
     public Job visitorJob() {
-        return jobBuilderFactory.get(VisitCode.VISITOR_JOB.getName())
-                .start(visitorJobStep(null))
+        return jobBuilderFactory.get(JOB_NAME)
+                .start(visitorJobStep())
+                .preventRestart()
                 .build();
     }
 
-    @Bean
+    @Bean(name = JOB_NAME + "_step")
     @JobScope
-    public Step visitorJobStep(@Value("#{jobParameters[createDate]}") String createDate) {
-        log.info(">>>>> Job Parameters {}" , createDate);
-
-        LocalDate localDate = LocalDate.parse(createDate , DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-
-        return stepBuilderFactory.get(VisitCode.VISITOR_JOB_STEP.getName())
+    public Step visitorJobStep() {
+        return stepBuilderFactory.get(JOB_NAME + "_step")
                 .tasklet(visitorTasklet)
                 .build();
     }
